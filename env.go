22068ecc0c3fe2c035e91847204f11774f010818
Abdul Dakkak <abduld@wolfram.com>
2017-03-02 10:00:14 -0600

    add env cmd

---
+++ env.go
@@ -0,0 +1,19 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var EnvCmd = &cobra.Command{
+	Use:    "env",
+	Short:  "Prints the environment variables.",
+	Hidden: false,
+	Run: func(cmd *cobra.Command, args []string) {
+		for _, env := range os.Environ() {
+			fmt.Println(env)
+		}
+	},
+}
