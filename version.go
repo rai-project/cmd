1add2830a28ee3b5bfe39a766dab553cbd035d72
Abdul Dakkak <abduld@wolfram.com>
2017-03-12 17:58:50 +0000

    work on release

--- version.go
+++ version.go
@@ -2,24 +2,36 @@ package cmd
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/spf13/cobra"
 )
 
 type VersionInfo struct {
-	BuildDate  string
+	// Version is populated at compile time by govvv from ./VERSION
+	Version string
+	// GitCommit is populated at compile time by govvv.
+	BuildDate string
+	// GitState is populated at compile time by govvv.
 	GitCommit  string
 	GitBranch  string
 	GitState   string
 	GitSummary string
 }
 
-var Version VersionInfo
+var Version = VersionInfo{
+	Version:    "0.2.0",
+	BuildDate:  time.Now().String(),
+	GitCommit:  "-dirty-",
+	GitBranch:  "-dirty-",
+	GitState:   "-dirty-",
+	GitSummary: "-dirty-",
+}
 
 // versionCmd represents the version command
 var VersionCmd = &cobra.Command{
 	Use:     "version",
-	Aliases: []string{"info", "v"},
+	Aliases: []string{"buildinfo"},
 	Short:   "Prints a the version information",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("BuildDate: ", Version.BuildDate)
